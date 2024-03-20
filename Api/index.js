d712b7fb830b8e3156ee6bc38e174de23c9d51d4
kareem <1998kareem@gmail.com>
2024-03-20 15:20:55 +0200

    create middleware and errorhandler functtion

--- Api/index.js
+++ Api/index.js
@@ -12,13 +12,22 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(err);
 })
 
-const app = express();
+const app = express()
 app.use(express.json())
 
 app.listen(3000, () => {
-    console.log("listening on port 3000");
+    console.log("listening on port 3000")
 })
 
 app.use("/api/user", UserRouter)
-
 app.use("/api/auth", AuthRouter)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || "Internal Server Error"
+    return res.status(statusCode).json({
+        succes: false,
+        statusCode,
+        message
+    })
+})
