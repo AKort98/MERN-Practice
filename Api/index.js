b35b86279893a65708a6f68461b440c7f00837b5
kareem <1998kareem@gmail.com>
2024-04-04 15:34:21 +0300

    update dist

--- Api/index.js
+++ Api/index.js
@@ -36,7 +36,7 @@ app.use("/api/listing", ListingRouter)
 app.use(express.static(path.join(__dirname, "/client/dist")))
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+    res.sendFile(path.join(__dirname, "Client", "dist", "index.html"))
 })
 
 
