57e257ce9707ff5c9b486e5c060cc84c8dd52867
kareem <1998kareem@gmail.com>
2024-04-05 20:56:49 +0300

    Add About page

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -48,25 +48,15 @@ export default function Home() {
   return (
     <div>
       {/* top */}
-      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
-        <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
-          Find your next <span className="text-slate-500">perfect</span>
+      <div className="flex flex-col gap-6 p-20 px-3 max-w-6xl mx-auto">
+        <h1 className="text-gray-300 font-bold text-3xl lg:text-6xl">
+          Find your next{" "}
+          <span className="bg-gradient-to-r from-green-400 to-green-800 text-transparent bg-clip-text">
+            Home
+          </span>
           <br />
-          place with ease
+          with ease
         </h1>
-        <div className="text-gray-400 text-xs sm:text-sm">
-          Embark on your journey to find the perfect home with Property Quest.
-          Our intuitive platform simplifies the search process, offering a
-          curated selection of houses tailored to your preferences.
-          <br />
-          We have a wide range of properties for you to choose from.
-        </div>
-        <Link
-          to={"/search"}
-          className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
-        >
-          Let's get started...
-        </Link>
       </div>
 
       {/* swiper */}
